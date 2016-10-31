9e38fd87d5b487b8823b79cc3a81a69da515bbfe
Mapkuff <map.siwapun@gmail.com>
2016-10-31 19:05:36 +0700

    finish UUIDImageIdFactory

--- src/test/java/com/prime/web/thumbnailer/test/unit/factory/UUIDImageIdFactoryTest.java
+++ src/test/java/com/prime/web/thumbnailer/test/unit/factory/UUIDImageIdFactoryTest.java
@@ -1,11 +1,15 @@
 package com.prime.web.thumbnailer.test.unit.factory;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.util.Assert;
+
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
 
 import io.prime.web.thumbnailator.factory.ImageIdFactory;
 import io.prime.web.thumbnailator.factory.UUIDImageIdFactory;
 
+@RunWith(value=DataProviderRunner.class)
 public class UUIDImageIdFactoryTest 
 {
 	private ImageIdFactory factory = new UUIDImageIdFactory();
@@ -16,7 +20,6 @@ public class UUIDImageIdFactoryTest
 		String fileName = "test.jpg";
 		String imageId = factory.generate(fileName);
 		Assert.isTrue(imageId.endsWith(fileName));
-		
+		Assert.isTrue(6 == imageId.split("/").length);
 	}
-
 }
