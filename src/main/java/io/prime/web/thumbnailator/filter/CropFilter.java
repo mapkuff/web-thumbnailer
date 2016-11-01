e104191d62cc0048866ba4b27a80a3bf486f0112
Mapkuff <map.siwapun@gmail.com>
2016-11-01 15:22:11 +0700

    fix CropFilter

--- src/main/java/io/prime/web/thumbnailator/filter/CropFilter.java
+++ src/main/java/io/prime/web/thumbnailator/filter/CropFilter.java
@@ -1,16 +1,35 @@
 package io.prime.web.thumbnailator.filter;
 
+import org.springframework.util.Assert;
+
 import net.coobird.thumbnailator.Thumbnails.Builder;
-import net.coobird.thumbnailator.geometry.Coordinate;
+import net.coobird.thumbnailator.geometry.Position;
+import net.coobird.thumbnailator.geometry.Positions;
 
 public class CropFilter implements ThumbnailatorFilter
 {
 	private int width;
 	private int height;
+	private Position position;
 
 	public <T> void filter(Builder<T> builder) 
 	{
-		builder.crop(new Coordinate(this.width, this.height));
+		Assert.isTrue(this.width > 0 || this.height > 0);
+		if (this.width > 0 && this.height > 0) {
+			builder.size(width, height);
+		} else {
+			if ( this.width > 0 ) {
+				builder.width(this.width);
+			} else {
+				builder.height(this.height);
+			}
+		}
+		
+		if (null == this.position) {
+			this.position = Positions.CENTER;
+		}
+		
+		builder.crop(position);
 	}
 
 	public int getWidth() {
